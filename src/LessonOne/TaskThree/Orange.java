f7e6436c3ab30b14328921c307bc0834d2571e9f
Vjacheslava Kobozev <kob-slava@yandex.ru>
2020-02-18 12:49:59 +0300

    изменении доступности полей

--- src/LessonOne/TaskThree/Orange.java
+++ src/LessonOne/TaskThree/Orange.java
@@ -1,8 +1,8 @@
 package LessonOne.TaskThree;
 
 public class Orange extends Fruit {
-    private  String name = "Апельсин";
-    private float weight = 1.5f;
+    protected   String name = "Апельсин";
+    protected float weight = 1.5f;
 
     public float getWeight() {
         return weight;
