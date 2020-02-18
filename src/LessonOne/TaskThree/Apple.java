cf16cf6d3af80ae9a979ef73812cc0f54d70db1e
Vjacheslava Kobozev <kob-slava@yandex.ru>
2020-02-18 12:47:16 +0300

    логика класса яблока

--- src/LessonOne/TaskThree/Apple.java
+++ src/LessonOne/TaskThree/Apple.java
@@ -1,4 +1,14 @@
 package LessonOne.TaskThree;
 
-public class Apple {
+public class Apple extends Fruit {
+    private String name = "Яблоко";
+    private float weight = 1.0f;
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
