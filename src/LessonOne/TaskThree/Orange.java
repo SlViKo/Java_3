b439249f52d15f2ffd51ca9da60f494c6b510ae9
Vjacheslava Kobozev <kob-slava@yandex.ru>
2020-02-18 12:47:46 +0300

    логика класса апельсина

--- src/LessonOne/TaskThree/Orange.java
+++ src/LessonOne/TaskThree/Orange.java
@@ -1,4 +1,14 @@
 package LessonOne.TaskThree;
 
-public class Orange {
+public class Orange extends Fruit {
+    private  String name = "Апельсин";
+    private float weight = 1.5f;
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
